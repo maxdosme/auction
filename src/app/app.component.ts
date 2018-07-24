8ab9f042e93c006e416ca9e95a31bfd89d4bc4ac
233705835@qq.com <233705835@qq.com>
2018-07-24 15:00:14 +0800

    添加第三方插件jquery、bootstrap

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { text } from '../../node_modules/@angular/core/src/render3/instructions';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+
+  ngOnInit(): void {
+    //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
+    //Add 'implements OnInit' to the class.
+   
+  }
 }
